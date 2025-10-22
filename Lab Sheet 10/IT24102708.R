b7481a814b247e24de14b82f256bc14dccfc9755
IT24102708 <it24102708@my.sliit.lk>
2025-10-22 23:12:05 +0530

    Lab - 10

---
+++ Lab Sheet 10/IT24102708.R
@@ -0,0 +1,22 @@
+
+# IT24102708
+# Yovinma.P.B
+
+# Lab Sheet 10 
+
+
+getwd()
+setwd("C:\\Users\\Bisandi\\Desktop\\3rd Semester\\PS\\Practical\\Lab 10")
+getwd()
+
+
+## Exercise
+
+
+# Part (ii)
+
+observed_counts <- c(120, 95, 85, 100)
+probabilities <- c(0.25, 0.25, 0.25, 0.25)
+chisq.test(x = observed_counts, p = probabilities)
+
+
